9fb94fcd148d8fad2d8eac202af30fc4e3e872d8
EXCALIBUR <EXCALIBUR@DESKTOP-09B9E1C>
2025-03-09 18:06:47 +0300

    Add project files.

---
+++ MvcCv/Controllers/HobiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+using MvcCv.Repositories;
+namespace MvcCv.Controllers
+{
+    public class HobiController : Controller
+    {
+        GenericRepository<TblHobilerim> linq = new GenericRepository<TblHobilerim>();
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var hobiler = linq.List();
+            return View(hobiler);
+        }
+        [HttpPost]
+        public ActionResult Index(TblHobilerim p)
+        {
+            var t = linq.Find(x => x.ID == 1);
+            t.Aciklama1 = p.Aciklama1;
+            t.Aciklama2 = p.Aciklama2;
+            linq.TUpdate(t);
+            return RedirectToAction("Index");
+        }
+    }
+}
